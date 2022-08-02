a88312e5c04efb1d333f9e1a11c6c98e782f3046
PeithonKing <75820716+PeithonKing@users.noreply.github.com>
2022-08-02 09:20:32 +0530

    abstract, bib and organising into deiiferent folders

--- clean.py
+++ clean.py
@@ -1,39 +1,66 @@
-from os import listdir, remove
+import os
 
-with open(".gitignore") as f:
-    imp_patterns = [".git"] + [x[1:] for x in f.read().split()[1:]]
+imp_patterns = [".git*",
+               "keep*",
+               "*.tex",
+               "*.jpg",
+               "*.png",
+               "*.bib",
+               "clean.py"
+               ]
 
-def search(exps, s):
-    exps = list(exps)
-    s = list(s)
-    while len(exps) and len(s):
-        # print("".join(exps), "\t\t", "".join(s))
-        if exps[0] == "*":
-            if len(exps) == 1:
-                return True
-            if s[0] == exps[1]:
-                exps.pop(0)
-                exps.pop(0)
-            s.pop(0)
-                
+def match(pattern, string):
+    # print(pattern, string)
+    if pattern == "*":
+        return True  # if pattern is "*" match all
+    if "*" not in pattern:
+        return pattern == string  # if pattern doesn't have a "*" match them directly
+    if pattern[0] == "*":
+        if not len(string):
+            return False
+        if pattern[1] == string[0]:
+            return match(pattern[2:], string[1:])
         else:
-            if s[0] == exps[0]:
-                exps.pop(0)
-                s.pop(0)
-            else:
-                return False
-    if not len(exps) or exps == ["*"]:
-        return True
-    return False
+            return match(pattern, string[1:])
+    else:
+        return pattern[0] == string[0] and match(pattern[1:], string[1:])
 
 def not_keep(file):
     for imp_pattern in imp_patterns:
-        if search(imp_pattern, file):
+        if match(imp_pattern, file):
             return False
     return True
 
-to_delete = [x for x in listdir() if not_keep(x)]
+with open(".gitignore") as f:
+    gitignore = [x for x in f.read().split("\n") if len(x) and not x.startswith("#")]
+def clean(path = ".", delete = True):
+    global gitignore
+    l = os.listdir(path)
+    to_delete = [x for x in l if not os.path.isdir(x) and not_keep(x)]
+    for i in to_delete:
+        if delete: os.remove(f"{path}/{i}")
+        g = f"{path}/{i}"[2:]
+        if g not in gitignore:
+            gitignore.append(g)
+    # print(f"Deleted {len(to_delete)} files in {path}")
+    dirs = [name for name in l if os.path.isdir(name) and ".git" not in name]
+    # print("One batch done\n")
+    for dir in dirs:
+        # print(f"looking into {path}/{dir}")
+        clean(f"{path}/{dir}", delete)
 
-for i in to_delete: remove(i)
-
-print(f"removed {len(to_delete)} files")
+if __name__ == "__main__":
+    
+    # Deleting with permission
+    if input("Delete unnecessary files? (y/n) ") == "y":
+        clean()
+        print("Deleted", len(gitignore), "files in Total")
+    else: clean(delete = False)
+    
+    
+    # making a .gitignore file
+    if input("Make a .gitignore file? (y/n) ") == "y":
+        gitignore = "\n".join(gitignore)
+        with open(".gitignore", "w") as f:
+            f.write(gitignore)
+    print(gitignore)
